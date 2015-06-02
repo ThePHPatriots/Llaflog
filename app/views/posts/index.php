644333202256daf2924afbf3f8a1cc32ce25ca74
nguri <nderim.guri1@gmail.com>
2015-06-02 09:32:32 +0200

    Some Fixes - if user is authenticated show buttons  - create post  - edit  - delete

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -4,13 +4,16 @@
 
 {% block main %}
 <h2>Latest Blog Posts</h2>
+{% if help.logged_in %}<small><a href="{{ HTTP_ROOT ~ 'posts/create' ~ post.id }}" class="btn btn-default btn-xs">Create a post</a></small>{% endif %}
+<hr>
 
 <ol>
 {% for post in posts %}
   <li>
     <h3><a href="{{ HTTP_ROOT ~ 'posts/show/' ~ post.id }}">{{ post.title }}</a>
-      <small><a href="{{ HTTP_ROOT ~ 'posts/edit/' ~ post.id }}" class="btn btn-default btn-xs">Edit</a></small>
-      <small><a href="{{ HTTP_ROOT ~ 'posts/delete/' ~ post.id }}" class="btn btn-danger btn-xs">Delete</a></small>
+      {% if help.logged_in %}<small><a href="{{ HTTP_ROOT ~ 'posts/edit/' ~ post.id }}" class="btn btn-default btn-xs">Edit</a></small>{% endif %}
+      {% if help.logged_in %}<small><a href="{{ HTTP_ROOT ~ 'posts/delete/' ~ post.id }}" class="btn btn-danger btn-xs">Delete</a></small><br>{% endif %}
+      	<h6><p>Created at: {{post.created_at}}</p></h6><hr>
     </h3>
   </li>
 {% endfor %}
